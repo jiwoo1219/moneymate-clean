8b2d024129ee597d8e9349e25653a4f178e3d559
jiwoo1219 <uuijiuua@gmail.com>
2025-12-06 00:32:58 +0900

    Add lastCheckDate and lastBoxDate fields to PetStateDto

--- src/main/java/com/moneymate/service/PetStateDto.java
+++ src/main/java/com/moneymate/service/PetStateDto.java
@@ -5,9 +5,14 @@ import java.time.LocalDate;
 
 @Data
 public class PetStateDto {
+
     private int level;
     private int xp;
     private int bones;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate lastCheckDate;
+
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate lastBoxDate;
 }
