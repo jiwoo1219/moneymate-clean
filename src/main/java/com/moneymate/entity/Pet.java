d3ffcf2515f14ab0d1d8946cdd7cd1a07a22017f
jiwoo1219 <uuijiuua@gmail.com>
2025-12-05 21:02:15 +0900

    Add @JsonIgnore annotation to user field

--- src/main/java/com/moneymate/entity/Pet.java
+++ src/main/java/com/moneymate/entity/Pet.java
@@ -19,6 +19,7 @@ public class Pet {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonIgnore
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
